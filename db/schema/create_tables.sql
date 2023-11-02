5529de7538cbae86bb677b6cab967438870b1570
N.I <iworksWeb@gmail.com>
2023-11-02 18:42:43 +0900

    feat: CREATE TABLEのSQLに、NOT NULL制約を追加しました。

--- db/schema/create_tables.sql
+++ db/schema/create_tables.sql
@@ -1,21 +1,21 @@
 -- チャンネルテーブル
 CREATE TABLE channels (
   channel_id bigint(20) PRIMARY KEY AUTO_INCREMENT,
-  channel_name varchar(30) UNIQUE
+  channel_name varchar(30) UNIQUE NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ジャンルテーブル
 CREATE TABLE genres (
   genre_id bigint(20) PRIMARY KEY AUTO_INCREMENT,
-  genre_name varchar(30) UNIQUE
+  genre_name varchar(30) UNIQUE NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- 番組テーブル
 CREATE TABLE programs (
   program_id bigint(20) PRIMARY KEY AUTO_INCREMENT,
-  title varchar(100) UNIQUE,
-  description TEXT,
-  series_or_single tinyint
+  title varchar(100) UNIQUE NOT NULL,
+  description TEXT NOT NULL,
+  series_or_single tinyint NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ジャンル登録テーブル
@@ -31,7 +31,7 @@ CREATE TABLE genre_registries (
 CREATE TABLE seasons (
   season_id bigint(20) PRIMARY KEY AUTO_INCREMENT,
   program_id bigint(20),
-  season_number int,
+  season_number int NOT NULL,
   FOREIGN KEY (program_id) REFERENCES programs(program_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -40,12 +40,12 @@ CREATE TABLE episodes (
   episode_id bigint(20) PRIMARY KEY AUTO_INCREMENT,
   program_id bigint(20),
   season_id bigint(20),
-  episode_number varchar(100),
-  title varchar(100),
-  description TEXT,
-  duration time,
-  release_date date,
-  view_count bigint(20),
+  episode_number varchar(100) NOT NULL,
+  title varchar(100) NOT NULL,
+  description TEXT NOT NULL,
+  duration time NOT NULL,
+  release_date date NOT NULL,
+  view_count bigint(20) NOT NULL,
   FOREIGN KEY (program_id) REFERENCES programs(program_id),
   FOREIGN KEY (season_id) REFERENCES seasons(season_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -57,9 +57,9 @@ CREATE TABLE broadcasts (
   program_id bigint(20),
   season_id bigint(20),
   episode_id bigint(20),
-  start_time DATETIME,
-  end_time DATETIME,
-  view_count bigint(20),
+  start_time DATETIME NOT NULL,
+  end_time DATETIME NOT NULL,
+  view_count bigint(20) NOT NULL,
   FOREIGN KEY (channel_id) REFERENCES channels(channel_id),
   FOREIGN KEY (program_id) REFERENCES programs(program_id),
   FOREIGN KEY (season_id) REFERENCES seasons(season_id),
