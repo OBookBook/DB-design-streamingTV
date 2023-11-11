589bc439818c5edab6bd69e2e73f88fd52aae074
N.I <iworksWeb@gmail.com>
2023-11-11 23:53:23 +0900

    style: SQLクエリを整形 quest-1.sql

--- queries/quest-1.sql
+++ queries/quest-1.sql
@@ -1,7 +1,6 @@
 -- エピソード視聴数トップ3のエピソードタイトルと視聴数を取得:
-SELECT
-  title AS エピソードタイトル,
-  view_count AS 視聴数
-FROM episodes
-ORDER BY view_count DESC
-LIMIT 3;
+SELECT title AS エピソードタイトル,
+       view_count AS 視聴数
+  FROM episodes
+ ORDER BY view_count DESC
+ LIMIT 3;
